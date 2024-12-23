52c4ca6f3fc215ed5fb7a2597875c861fc205d19
Dunken <jasondunken@yahoo.com>
2024-12-23 14:40:28 -0500

    initial commit

---
+++ rainwater-types/site.model.ts
@@ -0,0 +1,62 @@
+export type SiteObj = {
+    id: number;
+    site: SiteInformation | undefined;
+    features: string[] | undefined;
+    rows: string[][] | undefined;
+};
+
+export type SiteInformation = {
+    SiteCode: string;
+    SiteName: string;
+    SiteDescription: Description;
+    VariableInfo: VariableInformation[];
+    source: SourceInfo;
+};
+
+export type Description = {
+    Latitude: number;
+    Longitude: number;
+    Elevation: number;
+    VerticalDatum: string;
+    SiteType: string;
+    SiteNotes: string;
+};
+
+export type VariableInformation = {
+    VariableCode: string;
+    VariableName: string;
+    ValueType: string;
+    DataType: string;
+    GeneralCategory: string;
+    SampleMedium: string;
+    VariableUnitName: string;
+    VariableUnitType: string;
+    VariableUnitAbbr: string;
+    NoDataValue: number;
+    TimeSupport: string;
+    TimeSuppUnitAbbr: string;
+    TimeSupportUnitName: string;
+    TimeSupportUnitType: string;
+    MethodDesctiption: string;
+    MethodLink: string;
+    VerticalOffset: number;
+    SensorNotes: string;
+};
+
+export type SourceInfo = {
+    Organization: string;
+    SourceLink: string;
+    Citation: string;
+};
+
+export type mapLocation = {
+    siteId: string;
+    lat: number;
+    lng: number;
+    icon: any;
+};
+
+export type DataRow = {
+    id: string;
+    data: string[];
+};
